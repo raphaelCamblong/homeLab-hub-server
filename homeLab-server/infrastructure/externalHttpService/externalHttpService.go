8a462fa8af4fb076a43577e149c35964f7b3ff70
Raphael <camblong.raphael@epitech.eu>
2024-08-25 03:56:29 +0200

    [ADD] externalHttpService interface

--- homeLab-server/infrastructure/externalHttpService/externalHttpService.go
+++ homeLab-server/infrastructure/externalHttpService/externalHttpService.go
@@ -1,6 +1,23 @@
 package externalHttpService
 
 type ExternalHttpService interface {
-	GetiLO()
-	GetXenOrchestra()
+	GetRedfish() Redfish
+	GetXenOrchestra() any
+}
+
+type externalHttpService struct {
+	redfish Redfish
+	xo      any
+}
+
+func NewExternalHttpService(redfish Redfish, xen any) ExternalHttpService {
+	return &externalHttpService{redfish, xen}
+}
+
+func (e *externalHttpService) GetRedfish() Redfish {
+	return e.redfish
+}
+
+func (e *externalHttpService) GetXenOrchestra() any {
+	return e.xo
 }
