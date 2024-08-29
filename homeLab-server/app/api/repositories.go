063bebfa1a7c0d9645ed1c13975ebb02f73ae83f
Raphael <camblong.raphael@epitech.eu>
2024-08-29 17:45:31 +0200

    [ADD] cloud repositories and XO infra connection

---
+++ homeLab-server/app/api/repositories.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"homelab.com/homelab-server/homeLab-server/infrastructure"
+	"homelab.com/homelab-server/homeLab-server/internal/repositories"
+)
+
+type Repositories struct {
+	XenOrchestra repositories.XenOrchestraRepository
+	Status       repositories.StatusRepository
+	Ilo          repositories.ILORepository
+	Redfish      repositories.RedfishRepository
+	Cloud        repositories.CloudRepository
+}
+
+func NewRepositories(infra *infrastructure.Infrastructure) *Repositories {
+	repo := Repositories{
+		XenOrchestra: repositories.NewXenOrchestraRepository(infra.Cache, infra.ExternalHttpService.GetXenOrchestra(), infra.Db),
+		Status:       repositories.NewStatusRepository(infra.Db),
+		Redfish:      repositories.NewRedfishRepository(infra.Cache, infra.ExternalHttpService.GetRedfish()),
+	}
+	repo.Ilo = repositories.NewThermalRepository(repo.Redfish)
+	repo.Cloud = repositories.NewCloudRepository(repo.XenOrchestra)
+
+	return &repo
+}
