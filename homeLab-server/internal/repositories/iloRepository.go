f1ab81d16fb0b16f5a740c3800fcc4edd620d317
Raphael <camblong.raphael@epitech.eu>
2024-08-29 18:42:17 +0200

    [ADD] power repositories caching

--- homeLab-server/internal/repositories/iloRepository.go
+++ homeLab-server/internal/repositories/iloRepository.go
@@ -1,19 +1,27 @@
 package repositories
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"homelab.com/homelab-server/homeLab-server/infrastructure/cache"
 	"homelab.com/homelab-server/homeLab-server/internal/entities/ilo"
+	"time"
 )
 
 type ILORepository interface {
 	GetThermal() (*entities.ThermalEntity, error)
+	GetPower() (*entities.PowerEntity, error)
 }
 
 type iloRepository struct {
 	redfishRepository RedfishRepository
+	cache             cache.Database
 }
 
-func NewThermalRepository(redfishRepository RedfishRepository) ILORepository {
-	return &iloRepository{redfishRepository: redfishRepository}
+func NewThermalRepository(redfishRepository RedfishRepository, cache cache.Database) ILORepository {
+	return &iloRepository{redfishRepository: redfishRepository, cache: cache}
 }
 
 func (r *iloRepository) GetThermal() (*entities.ThermalEntity, error) {
@@ -21,4 +29,42 @@ func (r *iloRepository) GetThermal() (*entities.ThermalEntity, error) {
 		return nil, err
 	}
 	return r.redfishRepository.GetThermalData()
+}
+
+func (r *iloRepository) GetPower() (*entities.PowerEntity, error) {
+	powerCacheKey := "Ilo_power_data"
+	ctx := context.Background()
+
+	data, err := r.cache.GetClient().Get(ctx, powerCacheKey).Result()
+	if err == nil {
+
+		powerEntity, err := entities.UnmarshalPowerEntity([]byte(data))
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal cached power data: %w", err)
+		}
+		return powerEntity, nil
+	}
+
+	if !errors.Is(err, redis.Nil) {
+		return nil, err
+	}
+
+	if err := r.redfishRepository.UseSession(); err != nil {
+		return nil, err
+	}
+	powerEntity, err := r.redfishRepository.GetPowerData()
+	if err != nil {
+		return nil, err
+	}
+
+	marshaledData, err := powerEntity.Marshal()
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal power data: %w", err)
+	}
+
+	if err := r.cache.GetClient().Set(ctx, powerCacheKey, marshaledData, time.Minute*30).Err(); err != nil {
+		return nil, fmt.Errorf("failed to cache power data: %w", err)
+	}
+
+	return powerEntity, nil
 }
